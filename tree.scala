a99c97745702386c3b3b985ca879a93e36cf4630
tdieumeg <tdieumeg@student.42.fr>
2015-05-23 17:00:30 +0200

    logic functions, tree classes

---
+++ tree.scala
@@ -0,0 +1,23 @@
+class Rule(
+    lhs: LogicTree,
+    rhs: LogicTree)
+{
+    def getLeftValue(): Int = lhs.getValue()
+}
+
+trait LogicTree {
+    def getValue(): Int
+}
+
+class Node(
+    lhs: LogicTree,
+    rhs: LogicTree
+    op: Int, Int => Int) extends LogicTree
+{
+    def getValue(): Int = op(lhs.getValue(), rhs.getValue())
+}
+
+class Leaf(data: Data) extends LogicTree
+{
+    def getValue(): Int = data.getValue()
+}
