12e8021dd1ac83cb2c9706f20b0fff4f1894c828
tdieumeg <tdieumeg@student.42.fr>
2015-06-01 17:29:33 +0200

    lexer done

--- lexer.scala
+++ lexer.scala
@@ -1,22 +1,68 @@
+
+
+object TokenType extends Enumeration {
+    val Query, Fact, Rule = Value
+}
+class Token(val ttype: TokenType.Value, val data: String)
+{
+    def getTokenType() : TokenType.Value = ttype
+    def getData() : String = data
+}
+class LexerException(msg: String) extends RuntimeException(msg)
+class TooManyQueriesException(msg:String) extends LexerException(msg)
+class TooManyFactsException(msg:String) extends LexerException(msg)
+
 val query = "^\\s*\\?\\s*[A-Z]*\\s*$".r
 val fact = "^\\s*=\\s*[A-Z]*\\s*$".r
 val rule = "^\\s*!*\\s*\\(*\\s*!*\\s*[A-Z]\\s*\\)*\\s*(?:[+|^]\\s*\\(*\\s*!?\\s*[A-Z]\\s*\\)*\\s*)*(?:=>|<=>)\\s*!*\\s*\\(*\\s*!*\\s*[A-Z]\\s*\\)*\\s*(?:[+|^]\\s*\\(*\\s*!?\\s*[A-Z]\\s*\\)*\\s*)*$".r
 val comment = "#(?:.*)(?=\n)".r
 val emptyline = "(?<=(\n|^))(?:\\s*\n)|(?:\\s*\n)(?=$)".r
 
-val source = scala.io.Source.fromFile("testZaz.txt")
-val lines = try source.mkString finally source.close()
-
-val cleanedFile = emptyline.replaceAllIn(comment.replaceAllIn(lines, ""), "")
-
-val split = cleanedFile.split("\n")
-
-for (ln <- split)
+if (args.length < 1)
 {
-    ln match {
-        case rule() => println("rule")
-        case fact() => println("fact")
-        case query() => println("query")
-        case _ => println(Console.RED + "FAIL" + Console.RESET)
+    println("usage: scala expert_system.scala filename")
+    System.exit(1)
+}
+
+val source: (scala.io.BufferedSource) =
+try
+{
+    scala.io.Source.fromFile(args(0))
+}
+catch
+{
+    case e: java.io.FileNotFoundException =>
+    {
+        println(Console.RED + "Error: File " + args(0) + " not found." + Console.RESET)
+        System.exit(1)
+        null
     }
+}
+
+val lines = try source.mkString finally source.close()
+val cleanedFile = emptyline.replaceAllIn(comment.replaceAllIn(lines, ""), "")
+val split = cleanedFile.split("\n")
+try lexer catch
+{
+    case e: LexerException => println(e.getMessage)
+}
+
+def lexer: List[Token] =
+{
+    var list = List[Token]()
+    for (ln <- split)
+    {
+        ln match {
+            case query() => list = new Token(TokenType.Query, ln)::list
+            case fact() => list = new Token(TokenType.Fact, ln)::list
+            case rule() => list = new Token(TokenType.Rule, ln)::list
+            case _ =>
+            {
+                throw new LexerException(Console.RED + "Error at line: " + Console.RESET + ln)
+            }
+        }
+    }
+    if (list.groupBy(_.ttype).mapValues(_.size)(TokenType.Query) > 1) throw new TooManyQueriesException(Console.RED + "Error: Too many queries." + Console.RESET)
+    if (list.groupBy(_.ttype).mapValues(_.size)(TokenType.Fact) > 1) throw new TooManyFactsException(Console.RED + "Error: Too many facts definition." + Console.RESET)
+    list
 }
