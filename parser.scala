fe8ef3bfb019b32dbbae0004557547f0c725d6b7
tdieumeg <tdieumeg@student.42.fr>
2015-06-02 14:56:43 +0200

    nouvelles additions au parser

--- parser.scala
+++ parser.scala
@@ -2,25 +2,54 @@ package parser
 {
 
   import tree.Rule
+  import tree.Data
   import lexer.Token
+  import lexer.TokenType
 
   class Parser(list: List[Token])
   {
-    val imply = "([^<=>]+)(=>|<=>)([^<=>]+)\n".r
+    val imply = "([^<=>]+)(=>|<=>)([^<=>]+)".r
+    val rules = List[Rule]()
+    val datalist = List[Data]()
 
     def splitRule()
     {
       /*var rule = new Rule()*/
-      for (l <- list)
+     for (l <- list.filter(x => x.getTokenType() == TokenType.Rule))
+     {
+       val matches = imply.findAllMatchIn(l.getData)
+       matches.foreach {
+         m => println(m.group(1))
+       }
+     }
+    }
+
+    def splitQuery()
+    {
+      for (l <- list.filter(x => x.getTokenType() == TokenType.Query))
       {
-        l match {
-          case imply(group) => {
-            println("--->  " + group(1))
-          }
-          case _ => println("non matching")
+
+      }
+    }
+
+    def splitFact()
+    {
+      val fact = list.filter(x => x.getTokenType() == TokenType.Fact)(0).getData()
+      for (ln <- fact)
+      {
+        ln match {
+          case ('=' | ' ' | '\t' | '\n' | '\r') => ""
+          case _ => print(ln)
         }
       }
     }
-  }
 
+
+    def parse()
+    {
+      splitRule()
+      splitQuery()
+      splitFact()
+    }
+  }
 }
